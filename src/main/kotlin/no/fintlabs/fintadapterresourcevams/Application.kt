8cf4a784e34de9f332ee0aa818dc411843a1438a
sindre <sindre.giske@novari.no>
2025-10-14 12:37:18 +0200

    initial commit

---
+++ src/main/kotlin/no/fintlabs/fintadapterresourcevams/Application.kt
@@ -0,0 +1,11 @@
+package no.fintlabs.fintadapterresourcevams
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FintAdapterResourceVamsApplication
+
+fun main(args: Array<String>) {
+    runApplication<FintAdapterResourceVamsApplication>(*args)
+}
