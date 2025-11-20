3d2b8d7365f467bdb0823c408d9b0bf32dc21508
sindre <sindre.giske@novari.no>
2025-11-20 12:09:26 +0100

    AdapterRegistration progress (Awaiting Beta update)

--- src/main/kotlin/no/fintlabs/fintadapterresourcevams/Application.kt
+++ src/main/kotlin/no/fintlabs/fintadapterresourcevams/Application.kt
@@ -3,11 +3,13 @@ package no.fintlabs.fintadapterresourcevams
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.ComponentScan
 import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
 @EnableScheduling
 @ConfigurationPropertiesScan
+@ComponentScan(basePackages = ["no.fintlabs.adapter"])
 class FintAdapterResourceVamsApplication
 
 fun main(args: Array<String>) {
