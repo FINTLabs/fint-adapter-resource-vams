386db0b6e6fa374fcc648b1b56c89c31acd468f9
sindre <sindre.giske@novari.no>
2026-09-07 12:47:09 +0200

    Removed temporary testing stuff

--- src/main/kotlin/no/fintlabs/fintadapterresourcevams/model/fint/datautstyr/digitalEnhet/DigitalEnhetPublisher.kt
+++ src/main/kotlin/no/fintlabs/fintadapterresourcevams/model/fint/datautstyr/digitalEnhet/DigitalEnhetPublisher.kt
@@ -1,11 +1,8 @@
 package no.fintlabs.fintadapterresourcevams.model.fint.datautstyr.digitalEnhet
 
-import jakarta.annotation.PostConstruct
 import kotlinx.coroutines.runBlocking
 import no.fintlabs.adapter.config.AdapterProperties
 import no.fintlabs.adapter.datasync.ResourcePublisher
-import no.fintlabs.adapter.datasync.SyncData
-import no.fintlabs.adapter.models.sync.SyncType
 import no.novari.fint.model.resource.ressurs.datautstyr.DigitalEnhetResource
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Service
@@ -16,16 +13,9 @@ abstract class DigitalEnhetPublisher(
     props: AdapterProperties
 ) : ResourcePublisher<DigitalEnhetResource, DigitalEnhetRepository>(repository, props) {
 
-    // TODO; runBLocking in the other Publishers instead of suspend.
-    //TODO: remove postConstruct and fixedDelay. They are only for testing.
-    @PostConstruct
-    @Scheduled(cron = "\${fint.cron}", fixedDelay = 600)
+    @Scheduled(cron = "\${fint.cron}")
     fun performFullSync() = runBlocking {
         println("\n :: Starting Fullsync of DigitalEnhet :: \n")
-        submit(
-//            repository.fetchResources()
-//            TODO: remove this after Auth testing complete
-            SyncData<DigitalEnhetResource>(emptyList<DigitalEnhetResource>(), SyncType.FULL)
-        )
+        submit(repository.fetchResources())
     }
 }
